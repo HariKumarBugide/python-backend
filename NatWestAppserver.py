b71d85d9f5b12ee23f47a10478ea850cc0bf3e85
HariKumarBugide <67994406+HariKumarBugide@users.noreply.github.com>
2020-08-05 04:29:00 -0400

    Update NatWestAppserver.py

--- NatWestAppserver.py
+++ NatWestAppserver.py
@@ -177,4 +177,4 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
